ef852be4492c609348f9722ab19035f709d74b42
Maria Eduarda <meac21kj@gmail.com>
2025-04-09 20:47:34 -0300

    feat(application): EmailSenderService

---
+++ src/main/java/com/meac/email_service/application/EmailSenderService.java
@@ -0,0 +1,18 @@
+package com.meac.email_service.application;
+
+import com.meac.email_service.adapters.EmailSenderGateway;
+import com.meac.email_service.core.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
--- src/main/java/com/meac/email_service/core/EmailSenderUseCase.java
+++ src/main/java/com/meac/email_service/core/EmailSenderUseCase.java
@@ -3,6 +3,6 @@ package com.meac.email_service.core;
 public interface EmailSenderUseCase {
 
     void sendEmail(String to, String subject, String body);
-    
+
 
 }
