35e0784dd89fd797f0dc8235b4af50c566e5b903
Maria Eduarda <meac21kj@gmail.com>
2025-04-09 22:08:47 -0300

    feat(controllers): EmailSenderController

---
+++ src/main/java/com/meac/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,39 @@
+package com.meac.email_service.controllers;
+
+import com.meac.email_service.application.EmailSenderService;
+import com.meac.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+
+    @Autowired
+    public EmailSenderController(final EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email sent successfully");
+        } catch (EmailServiceException e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+
+
+
+}
